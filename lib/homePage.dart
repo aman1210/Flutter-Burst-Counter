a3730c98a282850964e3ac55c85d229f73f0a5c0
aman1210 <aman.srivastava101@gmail.com>
2021-01-14 09:33:19 +0530

    adding physics for particles

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:burst_counter/particle.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,6 +24,24 @@ class _HomePageState extends State<HomePage> {
 
   Rect boxSize = Rect.zero;
 
+  Random random = Random();
+
+  List<Particle> particles = [];
+
+  double fps = 1 / 30;
+
+  Timer timer;
+
+  final double gravity = 9.81;
+  final double drag = 0.47;
+  final double density = 1.1644;
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
   @override
   void initState() {
     super.initState();
@@ -27,6 +49,72 @@ class _HomePageState extends State<HomePage> {
       Size size = _boxKey.currentContext.size;
       boxSize = Rect.fromLTRB(0, 0, size.width, size.height);
     });
+    timer = Timer.periodic(
+        Duration(milliseconds: (fps * 1000).floor()), frameBuilder);
+  }
+
+  frameBuilder(Timer timer) {
+    particles.forEach((pt) {
+      double dragForceX =
+          0.5 * density * pow(pt.velocity.x, 2) * drag * pt.area;
+      double dragForceY =
+          0.5 * density * pow(pt.velocity.y, 2) * drag * pt.area;
+      dragForceX = dragForceX.isInfinite ? 0.0 : dragForceX;
+      dragForceY = dragForceY.isInfinite ? 0.0 : dragForceY;
+      double accX = dragForceX / pt.mass;
+      double accY = dragForceY / pt.mass + gravity;
+      pt.velocity.x += accX * fps;
+      pt.velocity.y += accY * fps;
+
+      pt.position.x += pt.velocity.x * fps * 100;
+      pt.position.y += pt.velocity.y * fps * 100;
+      collision(pt);
+    });
+    setState(() {});
+  }
+
+  collision(Particle pt) {
+    //rigt wall
+    if (pt.position.x > boxSize.width - pt.radius) {
+      pt.position.x = boxSize.width - pt.radius;
+      pt.velocity.x *= pt.jumpFactor;
+    }
+
+    //left wall
+    if (pt.position.x < pt.radius) {
+      pt.position.x = pt.radius;
+      pt.velocity.x *= pt.jumpFactor;
+    }
+
+    //bottom wall
+    if (pt.position.y > boxSize.height - pt.radius) {
+      pt.position.y = boxSize.height - pt.radius;
+      pt.velocity.y *= pt.jumpFactor;
+    }
+  }
+
+  burstParticle() {
+    if (particles.length > 200) {
+      particles.removeRange(0, 75);
+    }
+    counterText['count'] += 1;
+    Color newColor = colors[random.nextInt(colors.length)];
+    counterText['color'] = newColor;
+
+    int count = random.nextInt(25).clamp(7, 25);
+    for (int i = 0; i < count; i++) {
+      Particle p = new Particle();
+      p.position = PVector(boxSize.center.dx, boxSize.center.dy);
+      double randomX = random.nextDouble() * 4;
+      if (i % 2 == 0) {
+        randomX = -randomX;
+      }
+      double randomY = random.nextDouble() * -8.0;
+      p.velocity = PVector(randomX, randomY);
+      p.radius = (random.nextDouble() * 10.0).clamp(2.0, 10.0);
+      p.color = newColor;
+      particles.add(p);
+    }
   }
 
   @override
@@ -51,13 +139,25 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
+            ...particles
+                .map((p) => Positioned(
+                      top: p.position.y,
+                      left: p.position.x,
+                      child: Container(
+                        width: p.radius * 2,
+                        height: p.radius * 2,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: p.color),
+                      ),
+                    ))
+                .toList()
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         backgroundColor: counterText['color'],
-        onPressed: () {},
+        onPressed: burstParticle,
       ),
     );
   }
